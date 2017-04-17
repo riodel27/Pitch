1b592218e36adb30945dc2efdbd26f5be31ede47
riodel27 <riopamaong@gmail.com>
2017-04-17 15:12:40 +0800

    style footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 
 gulp.task('sass', function(){
@@ -10,6 +11,14 @@ gulp.task('sass', function(){
 
 });
 
+gulp.task('scripts', function(){
+	return  gulp.src('js/**/*.js')
+					.pipe(concat('all.js'))
+					.pipe(uglify())
+					.pipe(gulp.dest('js'))
+});
+
 gulp.task('default', function(){
 	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('js/**/*.js',['scripts']);
 });
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  document.getElementById("countryList").addEventListener("click",displayCountryList);
+
+
+  function displayCountryList(){
+  document.getElementById("countryList").innerHTML = "Country </br> Brazil";
+  }
+
+
+});
